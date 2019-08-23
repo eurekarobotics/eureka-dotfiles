66ae26e2d1815e1e6f1c7259d77be6321508cedf
Hung Pham <hungpham2511@gmail.com>
2019-08-23 20:26:58 +0800

    initial commit

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -46,7 +46,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (dockerfile-mode ag diminish use-package))))
+ '(package-selected-packages (quote (yaml-mode dockerfile-mode ag diminish use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
