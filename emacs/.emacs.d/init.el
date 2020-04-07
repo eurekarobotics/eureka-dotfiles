5bd91256a42f7617becd6bc174654ea239d50576
Hung Pham <hungpham@eurekarobotics.com>
2020-04-07 11:05:31 +0000

    Add lsp-mode.

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -45,6 +45,15 @@
 	 )
   )
 
+(use-package lsp-mode
+  :hook (c++-mode . lsp)
+  :commands lsp
+  :ensure t
+  :config
+  (setq gc-cons-threshold 100000000)
+  (setq read-process-output-max (* 1024 1024)) ;; 1mb
+  )
+
 
 ;; Configure appearances
 (use-package theme
@@ -56,7 +65,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (yaml-mode dockerfile-mode ag diminish use-package))))
+ '(custom-safe-themes
+   '("63df625509cec7b2b7deba1d48ef07ffbee99c735a95d130ad9d1fc3df8b9a34" default))
+ '(package-selected-packages
+   '(lsp-mode yaml-mode dockerfile-mode ag diminish use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
